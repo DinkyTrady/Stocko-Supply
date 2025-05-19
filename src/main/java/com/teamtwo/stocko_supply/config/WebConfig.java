a88edf136f8979f74a700baffb0a2b30bc239391
DinkyTrady <DinkyTrady@kyra.com>
2025-05-19 14:01:16 +0700

    feat: Adding web configuration
    so people can't go to the homepage if not login

---
+++ src/main/java/com/teamtwo/stocko_supply/config/WebConfig.java
@@ -0,0 +1,28 @@
+package com.teamtwo.stocko_supply.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Autowired
+    private LoginInterceptor loginInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/**");
+    }
+
+    @Override
+    public void addResourceHandlers(@NonNull ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/webjars/**", "/static/**")
+                .addResourceLocations("/webjars", "/static")
+                .resourceChain(false);
+    }
+}
